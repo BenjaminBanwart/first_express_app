c1e7dd1caec40d455ee4f4df1eee4358043d0f4c
Benjamin Banwart <benjibanwart@gmail.com>
2022-02-21 19:07:56 -0600

    added bonus

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const app = express()
 
 // Create a homepage route.
 app.get('/', function (req, res) {
-    // This gets sent to the client
+    // This gets sent to the client 
     // (your web browser most likely!)
     res.send('Hello world')
+})
+
+// Create a second route.
+app.get('/second', function (req, res) {
+    res.send('My Second Page!')
 })
 
 // Listen for connections.
